64093ea1535c17149bcf12830b3d1dcac9ef266f
Alessandro Mercurio <126950427+AlessandroMercurio@users.noreply.github.com>
2023-03-11 18:11:18 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -1,6 +1,20 @@
 # My first code in Git Hub
+
 # Let's install the raster package 
-
 install.packages("raster") # outside R use ""
-
 library(raster) # call package
+
+# setting the working directory
+setwd("C:/lab/")
+
+# import and assign data 
+l2011 <- brick("p224r63_2011_masked.grd")
+
+# Plotting the data in a savage manner
+plot(l2011)
+
+cl <- colorRampPalette(c("red", "orange", "yellow")) (100) # to change color ramp in the image
+plot(l2011, col=cl) 
+
+# plotting one element
+plot(l2011[[4]], col=cl)
