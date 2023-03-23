5a4b14d56755bef14e05fc68518f39c33a12ee51
Alessandro Mercurio <126950427+AlessandroMercurio@users.noreply.github.com>
2023-03-23 17:35:55 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -19,6 +19,8 @@ plot(l2011, col=cl)
 # plotting one element
 plot(l2011[[4]], col=cl)
 
+
+
 # Exercise: change the colour gamut for all images
 cl <- colorRampPalette(c("cyan", "azure", "darkorchid", "aquamarine")) (100)
 plot(l2011, col=cl) 
@@ -58,6 +60,8 @@ plot(l2011[[3]], col=clr)
 clnir <- colorRampPalette(c("red", "orange", "yellow")) (100)
 plot(l2011[[4]], col=clnir)
 
+
+
 # RGB plotting
 plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
 plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
@@ -79,6 +83,47 @@ plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
 par(mfrow = c(2,1)) 
 plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
 plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
+
+
+
+# Exercise: plot the NIR band
+clnir <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(l2011[[4]], col=clnir)
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
+
+
+# Exercise: import the 1988 image
+l1988 <- brick("p224r63_1988_masked.grd")
+
+# Exercise: plot in RGB space (natural colours)
+plotRGB(l1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+
+# multiframe
+par(mfrow = c(2,1))
+plotRGB(l1988, 4, 3, 2, stretch="Lin")
+plotRGB(l2011, 4, 3, 2, stretch="Lin")
+plotRGB(l1988, 4, 3, 2, stretch="Hist")
+
+# multiframe with 4 images
+par(mfrow = c(2,2))
+plotRGB(l1988, 4, 3, 2, stretch="Lin")
+plotRGB(l2011, 4, 3, 2, stretch="Lin")
+plotRGB(l1988, 4, 3, 2, stretch="Hist")
+plotRGB(l2011, 4, 3, 2, stretch="Hist")
+
+
+
+
+
+
+
+
+
 
 
 
