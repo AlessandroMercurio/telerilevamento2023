0614e28b13e9b8a136df1d2f011806b05be0827e
Alessandro Mercurio <126950427+AlessandroMercurio@users.noreply.github.com>
2023-04-20 17:01:46 +0200

    Update R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -6,12 +6,12 @@ setwd("C:/lab/")
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 plotRGB(so, 1, 2, 3, stretch="lin")
 
-# Classifying the solar data
+## Classifying the solar data
 
 # 1. Get all the single values
 singlenr <- getValues(so)
 
-# 2. Classify
+# 2. Classify (kmeans)
 kcluster <- kmeans(singlenr, centers = 3)
 
 # 3. Set values to a raster on the basis of so
@@ -31,6 +31,36 @@ percentages = frequencies * 100 / tot
 # class 1: 21.21993
 # class 3: 37.33349
 # class 2: 41.44658
+
+####
+## Grand Canyon exercise 
+####
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+
+plotRGB(gc, 1, 2, 3, stretch="lin")
+
+# the image is quite big, let's crop it
+gcc <- crop(gc, drawExtent())
+plotRGB(gcc, 1, 2, 3, stretch="lin")
+
+# 1 Get values
+singlenr <- getValues(gcc)
+
+# 2 Classify
+kcluster <- kmeans(singlenr, centers = 3)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
