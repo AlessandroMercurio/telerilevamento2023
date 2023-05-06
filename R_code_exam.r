51070ab42180c50cfe5ccf5e82d4211ea8da32d2
Alessandro Mercurio <126950427+AlessandroMercurio@users.noreply.github.com>
2023-05-06 19:41:41 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -116,11 +116,31 @@ l22class <- setValues(l22[[1]], kcluster2$cluster)
 # Choosing a colorRampPalette for an optimal show off of the classes
 cl <- colorRampPalette(c("cornsilk", "aquamarine4", "burlywood", "darkolivegreen3", "chocolate")) (100)
 
-# Plotting several images in a Multiframe 
+# Plotting several images in a Multiframe with ggplot2 package
+
 par(mfrow=c(1,2))
 plot(l21class, col=cl, main="Year 2021")
 plot(l22class, col=cl, main="Year 2022") 
 
+
+g1 <- ggRGB (TP1987, r =1, g = 2, b = 3, stretch = "lin")
+
+g2 <- ggRGB (TP2021, r =1, g = 2, b = 3, stretch = "lin")
+
+# plot both in the same page 
+g1+g2
+dev.off()
+
+# save plot
+jpeg("g1+g2.jpg", 900, 900)
+plot(g1+g2, col=cl, main="RGB")
+dev.off()
+
+
+# Saving the image
+jpeg("l21vsl22.jpg", 900, 900)
+
+dev.off()
 
 
 # -----Class percentages
@@ -165,153 +185,56 @@ calculated_km2<- c(6.92709, 4.33067, 2.59642)
 
 Table2 <- data.frame(data,calculated_km2)
 View(Table2) 
-# multiframe of the images to visualize them both in the same plot 
 
-cl <- colorRampPalette(c("#FFFFCC","#C7E9B4","#7FCDBB","#40B6C4","#2C7FB8" ,"#253494")) (100)
+#DVI 
+
+dvi21 = al21[[4]] - al21[[3]]
+dvi22 = al22[[4]] - al22[[3]]
+
+cl <- colorRampPalette(c('darkblue', 'yellow', 'red', 'black')) (100)
 par(mfrow=c(1,2))
-plot(TP1987, main="1999")
-plot(TP2021, main="2021")
+plot(dvi21, col=cl)
+plot(dvi22, col=cl)
+
+#NDVI
+ndvi21 = dvi21/(al21[[4]]+ al21[[3]])
+ndvi22 = dvi22/(al22[[4]]+ al22[[3]])
+
+par(mfrow=c(1,2))
+plot(ndvi21, col=cl)
+plot(ndvi22, col=cl)
 dev.off()
 
-# visualize the images with RGB colors 
-# red = band 1
-# green = band 2 
-# blue= band 3
+# difference of dvi and ndvi(multitemporal analysis)
 
-g1 <- ggRGB (TP1987, r =1, g = 2, b = 3, stretch = "lin")
+difdvi = dvi21 - dvi22
+cld <- colorRampPalette(c('blue', 'white', 'red')) (100)
+plot(difdvi, col=cld)
 
-g2 <- ggRGB (TP2021, r =1, g = 2, b = 3, stretch = "lin")
-
-# plot both in the same page 
-g1+g2
-dev.off()
-
-# save plot
-jpeg("g1+g2.jpg", 900, 900)
-plot(g1+g2, col=cl, main="RGB")
-dev.off()
+difndvi = ndvi21 - ndvi22
+plot(difndvi, col=cld)
 
 
-###### 2. Semiquantitative calculation of the water surface loss using unsupervised classification ######
-TP1987_pca <- rasterPCA(TP1987)
-
-# Visualize the importance of components 
-summary(TP1987_pca$model)
-
-#Importance of components:
-#Comp.1      Comp.2      Comp.3
-#Standard deviation     112.849604 15.52086859 3.920285709
-#Proportion of Variance   0.980274  0.01854298 0.001182995
-#Cumulative Proportion    0.980274  0.99881701 1.000000000
-
-# plot a pca map (the PC1 component has 98,02% of variance)
-plot(P1999_pca$map)
-dev.off()
-
-# assign a name for all the pca components
-pc1_1999 <- P1999_pca$map$PC1
-pc2_1999 <- P1999_pca$map$PC2
-pc3_1999 <- P1999_pca$map$PC3
-
-# plot of the components with viridis 
-g1 <- ggplot() + 
-  geom_raster(P1999_pca$map, mapping=aes(x=x, y=y, fill=PC1)) + 
-  scale_fill_viridis(option = "mako") +
-  ggtitle("PC1")
+nir8A <- raster("B8A21.tif")
+ swir<- raster("B12.tif")
+ alm21 <- stack(nir8A, swir)
+ View(alm21)
+ View(alm21)
+ alm21
 
 
-g2 <- ggplot() + 
-  geom_raster(P1999_pca$map, mapping=aes(x=x, y=y, fill=PC2)) + 
-  scale_fill_viridis(option = "mako") +
-  ggtitle("PC2")
-
-g3 <- ggplot() + 
-  geom_raster(P1999_pca$map, mapping=aes(x=x, y=y, fill=PC3)) + 
-  scale_fill_viridis(option = "mako") +
-  ggtitle("PC3")
-
-# all the components in the same plot 
-g1+g2+g3
-dev.off()
-
-# save plot
-jpeg("PC_1999.jpg", 900, 300)
-plot(g1+g2+g3, col=cl, main="Principal component analysis")
-dev.off()
-
-#Principal component analysis to extract data with many 
-#variables and create visualizations to display that data (PCA)
-#for 2021 image 
-P2021_pca <- rasterPCA(P2021)
-
-# Visualize the importance of components 
-summary(P2021_pca$model)
-#Importance of components:
-#Comp.1      Comp.2      Comp.3
-#Standard deviation     95.1994343 16.48198595 3.859144962
-#Proportion of Variance  0.9693514  0.02905572 0.001592923
-#Cumulative Proportion   0.9693514  0.99840708 1.000000000
-
-# plot a pca map (the PC1 component has 96,93% of variance)
-plot(P2021_pca$map)
-dev.off()
-#assign a name for all the pca components
-pc1_2021 <- P2021_pca$map$PC1
-pc2_2021 <- P2021_pca$map$PC2
-pc3_2021 <- P2021_pca$map$PC3
-
-# plot of the components with viridis
-im1 <- ggplot() + 
-  geom_raster(P2021_pca$map, mapping=aes(x=x, y=y, fill=PC1)) + 
-  scale_fill_viridis(option = "mako") +
-  ggtitle("PC1")
-
-im2 <- ggplot() + 
-  geom_raster(P2021_pca$map, mapping=aes(x=x, y=y, fill=PC2)) + 
-  scale_fill_viridis(option = "mako") +
-  ggtitle("PC2")
-
-im3 <- ggplot() + 
-  geom_raster(P2021_pca$map, mapping=aes(x=x, y=y, fill=PC3)) + 
-  scale_fill_viridis(option = "mako") +
-  ggtitle("PC3")
-
-# all the components in the same plot
-im1+im2+im3
-dev.off()
-
-# save plot
-jpeg("PC_2021.jpg", 900, 300)
-plot(im1+im2+im3, col=cl, main="Principal component analysis")
-dev.off()
-
-####### Calculate the difference between the first components#######
-diff <- pc1_1999 - pc1_2021
-
-# assign a color palette 
-cl <- colorRampPalette(c("purple", "yellow","orange","red")) (100)
-
-# plot the difference between the first and the second component
-plot(diff, col=cl, main = "Ice mass variation")
-dev.off()
-
-# save plot
-jpeg("difference.jpg", 900, 900)
-plot(diff, col=cl, main="Difference")
-dev.off()
+ ndmi21 = alm21[[1]] - alm21[[2]]/alm21[[1]] + alm21[[2]]
+ plot(ndmi21)
+ ndmi21 = (alm21[[1]] - alm21[[2]])/(alm21[[1]] + alm21[[2]])
+ ndmi21
 
 
-##### 3. Qualitative evaluation of drought impact on vegetation health through NDVI time series analysis #####
-# calculate heterogeneity in a 3x3 window for more definition
-sd3 <- focal(diff, matrix(1/9, 3, 3), fun=sd)
-sd3
+ plot(ndmi21)
+ nir8A22 <- raster("B8A22.tif")
+ swir22<- raster("B12_22.tif")
+ alm22 <- stack(nir8A22, swir22)
+ ndmi22 = (alm22[[1]] - alm22[[2]])/(alm22[[1]] + alm22[[2]])
+ ndmi22
 
-# plot a map so we can see all the areas with the highest heterogeneity
-cl <- colorRampPalette(c("magenta", "yellow", "green", "blue")) (100)
-plot(sd3, col=cl, main = "Standard deviation")
-dev.off()
+ plot(ndmi22)
 
-# save plot
-jpeg("sd3.jpg", 900, 900)
-plot(sd3, col=cl, main="Standard deviation")
-dev.off()
