2d65e6f7ce5685dcd68e017ea593e2ba155fc61c
Alessandro Mercurio <126950427+AlessandroMercurio@users.noreply.github.com>
2023-05-12 17:31:19 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -11,10 +11,21 @@ ssoil <- raster("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
 ssoil
 plot(ssoil)
 
-ggplot() +
-+ geom_raster(ssoil, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture))
+#create a dataframe to better use ggplot
+scd <- as.data.frame(ssoil, xy=T)
 
-# with viridis
 ggplot() +
-geom_raster(ssoil, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
-scale_fill_viridis(option="magma")
+geom_raster(scd, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture))+
+ggtitle("Soil Moisture from Copernicus")
+
+# Cropping an image
+ext <- c(23, 30, 62, 68) # define the extention
+sc.crop <- crop(ssoil, ext)
+
+# Excercise: plot via ggplot the cropped image
+scd.crop <- as.data.frame(sc.crop, xy=T)
+
+ggplot() +
+geom_raster(scd.crop, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture))+
+ggtitle("Soil Moisture cropped")+
+scale_fill_viridis(option="cividis")
