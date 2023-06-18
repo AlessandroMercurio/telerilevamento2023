71348815de8a26a126d0f5daa5cdc9a2c5a782ca
Alessandro Mercurio <126950427+AlessandroMercurio@users.noreply.github.com>
2023-06-18 10:49:59 +0200

    Update R_code_latex.R

--- R_code_latex.R
+++ R_code_latex.R
@@ -1,1 +1,43 @@
+# Create an article with Latex 
 
+\documentclass[a4paper,12pt]{article} # define the tipe of document and is structure
+
+\usepackage[utf8]{inputenc}  # for encoding
+\usepackage{graphicx} # package for graphs
+\usepackage{color} # for colors
+\usepackage{lineno} # for line numbers
+\usepackage{hyperref} # to create hyperlinks
+\usepackage{natbib} # for bibliography
+\usepackage{listings} # to insert external code into the code
+
+\documentclass[a4paper,12pt]{article}
+
+
+
+
+
+\linenumbers
+%  COLORI TESTO 
+%  items
+
+\title{This is my first LaTeX document!}
+\author{Duccio Rocchini $^1$, Student2 $^2$}
+
+% le funzioni in latex si indicano con \ e gli argomenti vanno all'interno di parentesi graffe
+
+\begin{document}
+\maketitle
+
+$^1$ Alma Mater Studiorum University of Bologna
+
+$^2$ NASA
+
+\tableofcontents
+
+\begin{abstract}
+Here is the abstract of my Master Thesis.
+
+The Master Thesis is dealing with something.
+
+Concluding, I made beautiful science.
+\end{abstract}
