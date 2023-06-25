0b53fde3b10417349cbe3324774649198c43a2de
Alessandro Mercurio <126950427+AlessandroMercurio@users.noreply.github.com>
2023-06-25 12:33:37 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -30,6 +30,6 @@ geom_raster(scd.crop, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture))+
 ggtitle("Soil Moisture cropped")+
 scale_fill_viridis(option="cividis")
 
-# head() mostra solo i primi elementi 
-# names() mostra il nome di un dataset
-# source() per caricare uno script da un file di testo
+# head() shows only the first elements
+# names() shows dataset name
+# source() to load script from a txt file
